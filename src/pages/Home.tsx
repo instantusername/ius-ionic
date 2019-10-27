e28f6731d20aafe0541670db1ea420728b14fc3f
umutcanbolat <10065235+umutcanbolat@users.noreply.github.com>
2019-10-28 00:05:14 +0300

    feat: add logo, appName and input

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,42 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
+import { IonContent, IonInput, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import styled from 'styled-components';
+import logo from '../assets/ius-logo.svg';
+
+const StyledBrand = styled.div`
+  width: fit-content;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-top: 35%;
+
+  h1 {
+    margin: 0px 0px 0.5rem;
+    font-size: 1.2rem;
+    font-weight: 700;
+    color: #5d5d5d;
+  }
+`;
+
+const StyledLogo = styled.img`
+  width: 3.2rem;
+`;
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
+      {/* <IonHeader>
         <IonToolbar>
           <IonTitle>Ionic Blank</IonTitle>
         </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
+      </IonHeader> */}
+      <IonContent>
+        <StyledBrand>
+          <StyledLogo alt="astronaut" className="astronaut" src={logo} />
+          <h1>Instant Username Search</h1>
+        </StyledBrand>
+        <IonInput placeholder="Search username"></IonInput>
       </IonContent>
     </IonPage>
   );
